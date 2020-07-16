a34c715b90c566a1bcdb51686e7c7f3c13b3089f
sahil-nayak <65059575+sahil-nayak@users.noreply.github.com>
2020-07-16 16:20:44 +0530

    Source code added

---
+++ GitWebAPIdemo/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitWebAPIdemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> getdata()
+        {
+            return new string[]{"Data1","Data2" };
+        }
+    }
+}
